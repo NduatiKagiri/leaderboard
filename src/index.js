550b9ea555127b8181dccf15e39504618eef06bd
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-06-17 02:30:14 +0300

    Add API and dynamic functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+import Scores from './modules/scores.js';
+
+const gameID = 'PGm0dj8ouOmWqOTT75Kk';
+
+const gameForm = document.getElementById('gameForm');
+const refresh = document.getElementById('refresh');
+
+gameForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  let playerName = document.getElementById('playerName').value;
+  let playerScore = document.getElementById('playerScore').value;
+
+  Scores.addScores(playerName, playerScore, gameID);
+
+  playerName = '';
+  playerScore = '';
+});
+
+refresh.addEventListener('click', Scores.requestScores);
+
+Scores.requestScores();
